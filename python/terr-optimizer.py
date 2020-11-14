14398f54f111a8b75babca8670101e4da58fbc13
hbarcello <hbarcello@tableau.com>
2020-11-13 16:54:00 -0800

    Functional working optimizer, poor man's annealing.

--- python/terr-optimizer.py
+++ python/terr-optimizer.py
@@ -6,12 +6,13 @@ import itertools
 import numpy as np
 from scipy.spatial import cKDTree
 from operator import itemgetter
-from statistics import variance
 import random
+import math
 
-debug=False
+debug = False
 
-def cluster_allocator(user_input_df, n_territories=9, n_iterations=500, decay_rate=.5):
+
+def cluster_allocator(user_input_df, n_territories=9, starting_temperature=1500, decay_rate=.5, iterations=50000):
     # Field Maps from cluster_engine
     # ClusterName | Index | Latitude | Longitude
     allocation_frame = user_input_df.groupby('ClusterName').agg({'Index': 'sum', 'Latitude': 'mean',
@@ -19,7 +20,7 @@ def cluster_allocator(user_input_df, n_territories=9, n_iterations=500, decay_ra
 
     print(allocation_frame)
     # settings for annealing
-    initial_temp = 1000
+    initial_temp = starting_temperature
     final_temp = 1
     current_temp = initial_temp
     km = KMeans(n_clusters=n_territories)
@@ -29,7 +30,7 @@ def cluster_allocator(user_input_df, n_territories=9, n_iterations=500, decay_ra
     best_sse = balance_calculator(target=territory_target, territory_data_frame=allocation_frame)
     print(f"Seeding SSE is {best_sse}")
     print(f"Beginning Annealing...")
-    while current_temp > final_temp:
+    while iterations > 0:
         config_to_eval = swap_neighbors(allocation_frame, get_neighbors(allocation_frame))
         # print(current_temp, " Current Temperature")
         # print(balance_calculator(territory_target, config_to_eval))
@@ -38,11 +39,19 @@ def cluster_allocator(user_input_df, n_territories=9, n_iterations=500, decay_ra
             best_sse = incumbent_sse
             print(f"New Optimum, new best SSE is {best_sse}")
             allocation_frame = config_to_eval
-        current_temp -= decay_rate
-        if current_temp % 50 == 0: print(current_temp, "iterations left")
+            best_frame = config_to_eval # Stash the best dataframe seen so far to export to CSV
+        # elif (math.e ** -(incumbent_sse - best_sse) / current_temp) >= random.randint(0,40):
+        # Trying to work on how to create a reasonable probability decay curve, but this is a placeholder.
+        elif random.randint(0,40) == 0:
+            print("Trying a random config")
+            allocation_frame = config_to_eval
+
+        current_temp = current_temp * decay_rate
+        iterations -= 1
+        if iterations % 100 == 0: print(iterations, " iterations left")
     # TODO Drop Index from allocation_frame before joining
     # TODO  Drop Territories from user_input_df before joining
-    final_frame = pd.merge(left=user_input_df, right=allocation_frame, how="left", on='ClusterName')
+    final_frame = pd.merge(left=user_input_df, right=best_frame, how="left", on='ClusterName')
     return final_frame
 
 
@@ -56,13 +65,15 @@ def balance_calculator(target, territory_data_frame):
 def get_neighbors(territory_data_frame):
     neighbor_df = gpd.GeoDataFrame(territory_data_frame, geometry=gpd.points_from_xy(territory_data_frame.Longitude,
                                                                                      territory_data_frame.Latitude))
-    # Select Random Territory
-    territory_select = random.randint(0, neighbor_df['Territory'].max())
-    # This is probably a terrible way to do this
-    # BUT - creating two data frames, one that is going to receive a new cluster, one that will give
-    receiving_df = neighbor_df.loc[neighbor_df['Territory'] == territory_select]
-    giving_df = neighbor_df.loc[neighbor_df['Territory'] != territory_select]
-    distance_output = (ckdnearest(receiving_df, giving_df))
+    # Select Random Territory to give a cluster, must have more than 1 cluster
+    territory_count = neighbor_df.groupby('Territory').count().reset_index()
+    territory_count = territory_count[territory_count['Index'] > 1]
+    territory_select = random.choice(territory_count['Territory'].to_list())
+    # Create DataFrames to Receive and Give
+    receiving_df = neighbor_df.loc[neighbor_df['Territory'] != territory_select]
+    giving_df = neighbor_df.loc[neighbor_df['Territory'] == territory_select]
+    distance_output = (ckdnearest( giving_df, receiving_df))
+    # TODO LOGIC NEEDS TO GO HERE TO MAKE SURE TERRITORIES ARE NOT REDUCED PAST 0
     swap_to_make = distance_output.loc[distance_output['dist'].idxmin()]
     return swap_to_make[4], swap_to_make[6]
 
@@ -95,5 +106,5 @@ def ckdnearest(gdfA, gdfB, gdfB_cols=['ClusterName']):
 if __name__ == "__main__":
     input_file = "./test/example_input_cluster.csv"
     input_data_frame = ce.cluster_input(input_file)
-    input_df = ce.cluster_generator(input_data_frame, cluster_factor=15, debug=False)
+    input_df = ce.cluster_generator(input_data_frame, cluster_factor=20, debug=False)
     cluster_allocator(user_input_df=input_df).to_csv("./test_output_data.csv")

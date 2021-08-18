5cb757f220ffb37cae0f7c58e43f3f6b65acba8e
hbarcello <hbarcello@tableau.com>
2021-08-18 13:45:15 -0700

    Functional working optimizer, poor man's annealing.
    Also added a lot of extra command line flexibility around field mapping.

--- python/cluster_engine.py
+++ python/cluster_engine.py
@@ -8,15 +8,15 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-
+import random
+pd.options.mode.chained_assignment = None
 
 def cluster_generator(input_tab_df, cluster_factor=12, cluster_field='State', target_territories=8,
                       debug=False, row_unit='Postal', balance_unit='Index', sub_cluster='None', cluster_round=0):
-    #df_to_cluster = input_tab_df[input_tab_df['Index'] <= (target_mean * 0.65)]
-    #df_to_pass_through = input_tab_df[input_tab_df['Index'] > (target_mean * 0.65)]
-    #df_to_pass_through['ClusterName'] = df_to_pass_through[row_unit]
-    # At this stage, for experiment purposes, don't filter out any rows, leave those until the state clustering portion
-    df_to_cluster = input_tab_df
+
+    df_to_cluster = input_tab_df[input_tab_df['Territories'] > 1].copy()
+    df_to_pass_through = input_tab_df[input_tab_df['Territories'] <= 1].copy()
+    df_to_pass_through['ClusterName'] = df_to_pass_through['Postal']
 
     if debug:
         print("Initial Cluster Filtering Completed...")
@@ -27,33 +27,43 @@ def cluster_generator(input_tab_df, cluster_factor=12, cluster_field='State', ta
     for state in df_unique_states:
         if debug: print("Now Clustering", state)
         current_subset = df_to_cluster[df_to_cluster[cluster_field] == state].copy()
-        target_territories = current_subset['Territories']
-        cluster_factor = current_subset['Clusters']
-        # Grab Territories and Clusters from fields in data frame copy
-
+        target_territories = current_subset['Territories'].max()
+        cluster_factor = current_subset['Clusters'].max()
         # Ensure at least 2 clusters per state, more if applicable, but no more than total postal codes
         numeric_cluster_maximum = current_subset[row_unit].count() - 1
-        target_mean = (current_subset['Index'].sum() / min(target_territories))
+        target_mean = (current_subset['Index'].sum() / target_territories)
+        units_to_skip_clustering = current_subset[current_subset['Index'] > (target_mean * 0.9)]
+        units_to_skip_clustering['ClusterName'] = units_to_skip_clustering.loc[:, ['Postal']]
+        df_to_pass_through = pd.concat([df_to_pass_through, units_to_skip_clustering])
+        current_subset = current_subset[current_subset['Index'] <= (target_mean * 0.9)]
+
         if debug:
             print("Target Territory Index is ", "{:.2f}".format(target_mean))
             print(type(target_mean))
             print(current_subset.info())
 
-        current_subset_int = current_subset.Index.sum()
-
         numeric_cluster_recommendation = min(int(current_subset.Index.sum() / target_mean) * cluster_factor,
                                              numeric_cluster_maximum)
         estimated_number_clusters = max(numeric_cluster_recommendation, 2)
+        # If Clusters are greater than unique latitude + longitude combinations, add random digits to end of lat-long
+        if current_subset['Latitude'].nunique() < estimated_number_clusters:
+            if estimated_number_clusters > current_subset.count()[0]:
+                estimated_number_clusters = current_subset.count()[0] - 1
+            else:
+                for x, row in current_subset.iterrows():
+                    current_subset.at[x, 'Latitude'] = row.Latitude + random.randint(1, 9999)/100000.0
+                    current_subset.at[x, 'Longitude'] = row.Longitude + random.randint(1, 9999)/100000.0
+
         if debug: print(estimated_number_clusters, "Estimated Clusters for", state)
         # Only cluster if there is more than one postal code in the group (data quality guard)
         if current_subset[row_unit].count() > 1:
             kmeans = KMeans(n_clusters=estimated_number_clusters)
             state_clusters = kmeans.fit_predict(current_subset[['Latitude', 'Longitude']])
-            if debug: print(state_clusters)['ClusterName'] = [state + "." + str(cluster_round) + "." + str(c + 1) for c in
+            if debug: print(state_clusters)
+            current_subset['ClusterName'] = [state + "." + str(cluster_round) + "." + str(c + 1) for c in
                                              state_clusters]
-            current_subset
         else:
-            current_subset['ClusterName'] = state + "." + str(cluster_round) + "." + current_subset[row_unit]
+            current_subset['ClusterName'] = state + "." + str(cluster_round) + "." + str(current_subset[row_unit])
         clusters_to_breakup = current_subset.groupby('ClusterName').sum()
         clusters_to_breakup = clusters_to_breakup[clusters_to_breakup['Index'] >= target_mean]
         if len(clusters_to_breakup) > 0:
